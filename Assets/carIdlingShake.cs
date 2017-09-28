5df0c390745f9f17d0bd142eebc28adaf4145ba7
tedbarnett <mail@tedbarnett.com>
2017-09-28 00:00:25 -0700

    Red car exhaust

--- Assets/carIdlingShake.cs
+++ Assets/carIdlingShake.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// See documentation at http://www.pixelplacement.com/itween/documentation.php
-// use any of the parameters for the ShakePosition method below
 public class carIdlingShake : MonoBehaviour {
     public GameObject target;
 	// Use this for initialization
 	void Start () {
-        iTween.ShakePosition(target, iTween.Hash("y", .010, "easeType", "spring", "loopType", "loop"));
+        iTween.ShakePosition(target, iTween.Hash("y", .01, "easeType", "spring", "loopType", "loop", "time", 1.0));
     }
 
     // Update is called once per frame
