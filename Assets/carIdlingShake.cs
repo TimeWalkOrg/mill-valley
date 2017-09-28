941835020266173d50a223803926dead9dd987c7
tedbarnett <mail@tedbarnett.com>
2017-09-27 23:39:12 -0700

    Car idling animation

---
+++ Assets/carIdlingShake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// See documentation at http://www.pixelplacement.com/itween/documentation.php
+// use any of the parameters for the ShakePosition method below
+public class carIdlingShake : MonoBehaviour {
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+        iTween.ShakePosition(target, iTween.Hash("y", .010, "easeType", "spring", "loopType", "loop"));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
