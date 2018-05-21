60421ada28add81195d607a939a40e00b3cc9a9f
tedbarnett <mail@tedbarnett.com>
2018-05-20 17:22:08 -0700

    Water splash effects

--- Assets/rotateWheel.cs
+++ Assets/rotateWheel.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rotateWheel : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public float rotationSpeed;
+    // Use this for initialization
+    void Start () {
 		
 	}
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(-(40 * (Vector3.right * Time.deltaTime)));
+        this.transform.Rotate(-(rotationSpeed * (Vector3.right * Time.deltaTime)));
     }
 }
