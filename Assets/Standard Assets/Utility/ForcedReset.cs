30b092aa9b7fca99c1205b5867335583947d94ab
tedbarnett <mail@tedbarnett.com>
2020-05-19 18:33:55 -0700

    Fixed 2 compile errors.  Still see iTween.cs problem.

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,9 +1,13 @@
+//ForcedReset.cs
+
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+//change GUITexture to Image
+[RequireComponent(typeof(Image))]
 
-[RequireComponent(typeof (GUITexture))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
